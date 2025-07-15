1dd73a5a6b105a69ed7003d08ea6b0bffeafc606
aryan3110A <prajapatiaryan3103@gmail.com>
2025-07-15 15:20:21 +0530

    aryan

--- src/config/tailwind/screens.ts
+++ src/config/tailwind/screens.ts
@@ -1,10 +1,11 @@
 export const screens = {
-  'xs': '475px',
-  'sm': '640px',
-  'md': '768px',
-  'lg': '1024px',
-  'xl': '1280px',
-  '2xl': '1536px',
-  '3xl': '1600px',
-  '4xl': '1920px',
+  
+    "mb":{max:"640px"},
+    "mobile":"350px",
+    "tablet":"768px",
+    "sm-laptop": "1080px",
+    "md-laptop": "1440px", // For 15.6-inch & 16-inch laptops
+    "lg": "1600px", 
+    "lg-laptop": "1600px",// Large PCs
+  
 }; 
